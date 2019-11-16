d9ca788002714d2c93af2e8419ac736514c09e91
snapshimi <54771286+snapshimi@users.noreply.github.com>
2019-11-16 19:32:36 +0330

    Update effect.js

--- js/effect.js
+++ js/effect.js
@@ -1,10 +1,15 @@
+var msg_sn = 0;
 function loadok() {
-    $("#loading").fadeOut();
-    if(document.getElementById("snapshimiframe").src == "http://snapshimi.com/"){
+    if (msg_sn == 0){
+        msg_sn = 1;
+        $("#loading").fadeOut();
+        $("#snlink").slideDown(1000);
+    }
+    /*if(document.getElementById("snapshimiframe").src == "http://snapshimi.com/"){
         $("#snlink").slideDown(1000);
     }else{
         $("#snlink").hide();   
-    }
+    }*/
 }
 
 function showsite() {
